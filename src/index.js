c8dddc20f02c27b6d55345b6dcf87cbcd29ee67e
Gilliard Santos <hello.codeindustry@gmailcom>
2022-06-17 13:23:58 -0300

    Heroku Deploy

--- src/index.js
+++ src/index.js
@@ -6,15 +6,9 @@ import App from './App';
 // Styles
 import './index.css'
 
-// Components
-import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// reportWebVitals(console.log)
-reportWebVitals();
